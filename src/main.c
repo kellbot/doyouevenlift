d011f229f8c10db78727de1a015b6b9ed1c8b703
kellbot <kellbot@gmail.com>
2014-05-16 18:38:39 -0700

    It scrolls!
    Scrolls through a hand-written list of strings. Yay.

--- src/main.c
+++ src/main.c
@@ -3,10 +3,28 @@
 Window *window;
 TextLayer *text_layer;
 char buffer[] = "00:00";
+typedef struct {
+  const char *activity;
+  unsigned int *reps;
+  unsigned int *weight;
+} Set;
 const char * string_list[] = {
-        "foo",
-        "bar",
-        "baz"
+        "Crunches",
+        "Side Oblique",
+        "Vertical Crunches",
+        "Bicycles",
+        "Leg Lifts",
+        "Torso Twists",
+        "Planks",
+        "Bench Press",
+        "Dumbbell Incline Press",
+        "Dumbbell Fly",
+        "Lateral Raise",
+        "Front Raise",
+        "Upright Row",
+        "Skullcrushers",
+        "Overhead Extensions",
+        "Tricep Pushdowns"
 };
 unsigned int exercise = 0;
 
@@ -24,8 +42,10 @@ void up_click_handler(ClickRecognizerRef recognizer, void *context)
  
 void down_click_handler(ClickRecognizerRef recognizer, void *context)
 {
+  unsigned int string_count = sizeof(string_list) / sizeof(string_list[0]);
+  
   //do nothing if we're on the last exercise
-  if (exercise >= sizeof(string_list)) return;
+  if (exercise >= string_count - 1) return;
   exercise = exercise + 1;
   update_exercise_name(string_list[exercise]);
 }
@@ -48,7 +68,6 @@ void window_load(Window *window)
   text_layer_set_background_color(text_layer, GColorClear);
   text_layer_set_text_color(text_layer, GColorBlack);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
 
   text_layer_set_text(text_layer, "Press A Button");
